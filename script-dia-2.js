ec52e6168cf7a578b525bf59aec559e4b8dab90e
Bruno Affonso <bidaffondo@gmail.com>
2021-02-25 14:48:08 -0400

    Commit com exercicios de conteudo 1, 2 e 3

---
+++ script-dia-2.js
@@ -0,0 +1,19 @@
+//Exercicios de conteúdo
+//1)
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+//2
+
+let indexOfPortfolio = menu.indexOf("Portfólio") ;
+
+console.log(indexOfPortfolio);
+
+//3
+
+menu.push("Contato");
+
+console.log(menu);
