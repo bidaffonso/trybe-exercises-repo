138958458b3931b0b019065e8814252539ee22eb
bidaffonso <bidaffondo@gmail.com>
2021-03-01 15:32:12 -0400

    Primeiro commit adicionando pasta do bloco 4 dia 4

---
+++ exercises/bloco4/js-dia-4/ex-fixacao.js
@@ -0,0 +1,14 @@
+let player = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: { golden: 2, silver: 3 },
+}
+
+console.log('A Jogadora ' + player.name + ' ' + player.lastName + ' tem ' + player.age + ' anos de idade.');
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor jogadora do mundo por ' + player.bestInTheWorld.length);
+
+console.log('A jogadora possui ' + player.medals.golden + ' medalhas de Ouro, e ' + player.medals.silver + ' medalas de Prata.');
