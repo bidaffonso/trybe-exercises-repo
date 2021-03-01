d786c39861cb478b86ab626492b21345296f8670
bidaffonso <bidaffondo@gmail.com>
2021-03-01 17:44:56 -0400

    atualizacao do exercicio de fixacao com funcoes e inicio de exercicios praticos

--- exercises/bloco4/js-dia-4/ex-fixacao.js
+++ exercises/bloco4/js-dia-4/ex-fixacao.js
@@ -1,3 +1,7 @@
+//Exercicios de fixacao do conteudo
+
+//Objetos
+
 let player = {
   name: 'Marta',
   lastName: 'Silva',
@@ -12,3 +16,13 @@ player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor jogadora do mundo por ' + player.bestInTheWorld.length);
 
 console.log('A jogadora possui ' + player.medals.golden + ' medalhas de Ouro, e ' + player.medals.silver + ' medalas de Prata.');
+
+function fullName (player) {
+  playerChange = player;
+  playerChange.fullName = playerChange.name + ' ' + playerChange.lastName;
+
+  return playerChange.fullName;
+}
+
+
+console.log('A Jogadora ' + fullName(player) + ' tem ' + player.age + ' anos de idade.');
