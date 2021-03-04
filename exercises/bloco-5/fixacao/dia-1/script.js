7547cd0f8fe51849c69a71924ec480f0a4fcb181
bidaffonso <bidaffondo@gmail.com>
2021-03-04 19:51:10 -0400

    Excluões e rename em pastas e arquivos

--- exercises/bloco-5/fixacao/dia-1/script.js
+++ exercises/bloco-5/fixacao/dia-1/script.js
@@ -2,7 +2,7 @@ document.getElementById('header-container').style.backgroundColor = 'darkgreen';
 
 document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'salmon';
 
-document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'darkkhaki';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'rgb(255, 234, 118)';
 
 for (let index = 0; index < document.querySelectorAll('.emergency-tasks h3').length ; index += 1) {
     document.querySelectorAll('.emergency-tasks h3')[index].style.backgroundColor = 'purple';
