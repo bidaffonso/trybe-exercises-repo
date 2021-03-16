cf90c6d5f25c842a74d680111f361d1aa162fb6b
bidaffonso <bidaffondo@gmail.com>
2021-03-16 18:34:09 -0400

    Commit com adiçao de estilos da biblioteca bulma

--- exercises/bloco-6/exercises/script.js
+++ exercises/bloco-6/exercises/script.js
@@ -1,5 +1,6 @@
 
 const state = document.getElementById('state');
+console.log(state);
 function createStates() {
   const stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
   for (let index = 0; index < stateOptions.length; index += 1) {
