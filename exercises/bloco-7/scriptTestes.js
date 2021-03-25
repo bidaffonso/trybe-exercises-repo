cd9ee6b3ec6453f6c80be82c310a5a8e92ef3e52
bidaffonso <bidaffondo@gmail.com>
2021-03-25 19:45:30 -0400

    Testes unitarios 3 e 4

--- exercises/bloco-7/scriptTestes.js
+++ exercises/bloco-7/scriptTestes.js
@@ -23,25 +23,71 @@
 
 //  Teste 2
 
+// const assert = require('assert');
+
+// function myRemove(arr, item) {
+//   let newArr = [];
+//   for (let index = 0; index < arr.length; index += 1) {
+//     if (item !== arr[index]) {
+//       newArr.push(arr[index]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// // implemente seus testes aqui
+
+// assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2 ,4]);
+// assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// const myList = [5, 6, 7, 8];
+// myRemove(myList, 5);
+// assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+
+// assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+//  Teste 3
+
+// const assert = require('assert');
+
+// function myRemoveWithoutCopy(arr, item) {
+//   for (let index = 0, len = arr.length; index < len; index += 1) {
+//     if (arr[index] === item) {
+//       arr.splice(index, 1);
+//       index -= 1;
+//       len -= 1;
+//     }
+//   }
+
+//   return arr;
+// }
+
+// // implemente seus testes aqui
+
+// assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+// assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+// const myList = [5, 6, 7, 8];
+// myRemoveWithoutCopy(myList, 5);
+// assert.deepStrictEqual(myList, [6, 7, 8]);
+// assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+//  Teste 4
+
 const assert = require('assert');
 
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
-    }
-  }
-  return newArr;
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
 }
 
 // implemente seus testes aqui
 
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2 ,4]);
-assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
+assert.strictEqual(myFizzBuzz(6), 'fizz');
+assert.strictEqual(myFizzBuzz(10), 'buzz');
+assert.strictEqual(myFizzBuzz(4), 4);
+assert.deepStrictEqual(myFizzBuzz('r'), false);
 
-const myList = [5, 6, 7, 8];
-myRemove(myList, 5);
-assert.deepStrictEqual(myList, [5, 6, 7, 8]);
-
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
