beaa0df690133795640825ef96ea582801c5df4c
bidaffonso <bidaffondo@gmail.com>
2021-03-29 19:19:37 -0400

    Resultado exercicio 2

--- exercises/bloco-8/exerc-dia-1.js
+++ exercises/bloco-8/exerc-dia-1.js
@@ -1,15 +1,36 @@
-const newEmployees = (employeeGen) => {
-  const employees = {
-    id1: employeeGen('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
-    id2: employeeGen('Luiza Drumond'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
-    id3: employeeGen('Carla Paiva')// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
-  }
-  return employees;
+//  Ex 1:
+
+// const newEmployees = (employeeGen) => {
+//   const employees = {
+//     id1: employeeGen('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+//     id2: employeeGen('Luiza Drumond'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+//     id3: employeeGen('Carla Paiva')// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+//   }
+//   return employees;
+// };
+
+// const employeeGen = (fullName) => {
+//   const email = { fullName, email: `${fullName.toLowerCase().split(' ').join('_')}@trybe.com` };
+//   return email;
+// };
+
+// console.log(newEmployees(employeeGen));
+
+//  Ex 2:
+
+const resultadoLoteria = (numAposta, checaNum) => {
+  const numSorteado = Math.floor((Math.random() * 5) + 1);
+  let resultado = '';
+  console.log(numSorteado);
+  if (checaNum(numAposta, numSorteado) === true) {
+    return 'Parabéns, você ganhou!';
+  } else {
+    return 'Tente novamente';
+  };
+  
 };
 
-const employeeGen = (fullName) => {
-  const email = { fullName, email: `${fullName.toLowerCase().split(' ').join('_')}@trybe.com` };
-  return email;
-};
+const checaNum = (numAposta, numSorteado) => numAposta === numSorteado;
 
-console.log(newEmployees(employeeGen));
+console.log(3);
+console.log(resultadoLoteria(3, checaNum));
